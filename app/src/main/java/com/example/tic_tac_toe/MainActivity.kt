a9c19b1998baa4b094d60aaa3bba08e36b986b7f
shayshalem <shshai007@gmail.com>
2025-01-26 00:26:22 +0200

    feat(tic-tac-toe): finish the game

--- app/src/main/java/com/example/tic_tac_toe/MainActivity.kt
+++ app/src/main/java/com/example/tic_tac_toe/MainActivity.kt
@@ -1,16 +1,155 @@
 package com.example.tic_tac_toe
 
+import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import com.example.tic_tac_toe.databinding.ActivityMainBinding
 
 class MainActivity : ComponentActivity() {
-    private lateinit var dataBinding : ActivityMainBinding
+    private lateinit var dataBinding: ActivityMainBinding
+    private var board = mutableListOf<Button>()
 
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+    companion object {
+        const val X = "X"
+        const val O = "O"
+    }
+
+    private var currentTurn = O;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         dataBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(dataBinding.root)
+
+        init()
+    }
+
+    private fun init() {
+        board.add(dataBinding.square11)
+        board.add(dataBinding.square12)
+        board.add(dataBinding.square13)
+        board.add(dataBinding.square21)
+        board.add(dataBinding.square22)
+        board.add(dataBinding.square23)
+        board.add(dataBinding.square31)
+        board.add(dataBinding.square32)
+        board.add(dataBinding.square33)
+
+        updateTitle()
+    }
+
+    fun handleSquareClicked(view: View) {
+        if (view !is Button) return
+
+        updateBoard(view)
+
+        if (hasPlayerWon(currentTurn)) result()
+
+        if (hasGameFinished()) result()
+
+        currentTurn = if (currentTurn == X) O else X;
+
+        updateTitle()
+    }
+
+    private fun updateBoard(clickedButton: Button) {
+        if (clickedButton.text != "") return
+
+        clickedButton.text = currentTurn;
+    }
+
+    private fun hasGameFinished(): Boolean
+    {
+        for(button in board)
+        {
+            if(button.text == "")
+                return false
+        }
+
+        return true
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updateTitle() {
+        dataBinding.currentTurn.text = currentTurn;
+    }
+
+    private fun hasPlayerWon(player: String): Boolean {
+        if (isBtnTextEquals(dataBinding.square11, player) &&
+            isBtnTextEquals(dataBinding.square12, player) &&
+            isBtnTextEquals(dataBinding.square13, player)
+        )
+            return true
+
+        if (isBtnTextEquals(dataBinding.square21, player) &&
+            isBtnTextEquals(dataBinding.square22, player) &&
+            isBtnTextEquals(dataBinding.square23, player)
+        )
+            return true
+
+        if (isBtnTextEquals(dataBinding.square31, player) &&
+            isBtnTextEquals(dataBinding.square32, player) &&
+            isBtnTextEquals(dataBinding.square33, player)
+        )
+            return true
+
+        if (isBtnTextEquals(dataBinding.square13, player) &&
+            isBtnTextEquals(dataBinding.square23, player) &&
+            isBtnTextEquals(dataBinding.square33, player)
+        )
+            return true
+
+        if (isBtnTextEquals(dataBinding.square11, player) &&
+            isBtnTextEquals(dataBinding.square21, player) &&
+            isBtnTextEquals(dataBinding.square31, player)
+        )
+            return true
+
+        if (isBtnTextEquals(dataBinding.square12, player) &&
+            isBtnTextEquals(dataBinding.square22, player) &&
+            isBtnTextEquals(dataBinding.square32, player)
+        )
+            return true
+
+        if (isBtnTextEquals(dataBinding.square11, player) &&
+            isBtnTextEquals(dataBinding.square22, player) &&
+            isBtnTextEquals(dataBinding.square33, player)
+        )
+            return true
+
+        if (isBtnTextEquals(dataBinding.square13, player) &&
+            isBtnTextEquals(dataBinding.square22, player) &&
+            isBtnTextEquals(dataBinding.square31, player)
+        )
+            return true
+
+        return false
+    }
+
+    private fun isBtnTextEquals(button: Button, symbol: String): Boolean = button.text == symbol
+
+    private fun result()
+    {
+        val message = "$currentTurn won"
+
+        AlertDialog.Builder(this)
+            .setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("Play again")
+            { _,_ ->
+                resetBoard()
+            }
+            .setCancelable(false)
+            .show()
+    }
+
+    private fun resetBoard() {
+        for(button in board)
+        {
+            button.text = ""
+        }
     }
 }
